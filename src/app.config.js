d3a67bf4a6ede6d06bfe5a374ec6b9ef175051e7
wangjue <348748267@qq.com>
2021-11-09 21:25:44 +0800

    fix: 修复头条小程序模板导航标题文字

--- src/app.config.js
+++ src/app.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  pages: ['pages/index/index'],
-  window: {
-    defaultTitle: 'Remax Toutiao Template',
-    titleBarColor: '#282c34',
-  },
+    pages: ['pages/index/index'],
+    window: {
+        navigationBarTitleText: 'Remax Toutiao Template',
+    },
 };
